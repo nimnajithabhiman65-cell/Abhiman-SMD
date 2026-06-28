518bb28e6048d134089bd83ea5ea8e8b66b0f061
nimnajithabhiman65-cell <nimnajithabhiman65@gmail.com>
2026-06-28 21:12:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "y0oiSCKJ#5QCiGGgtnq1E283yfY5_92NfGUzJ8XIM9zfN9Q17Lq4",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/nimnajithabhiman65-cell/Abhiman-SMD/blob/main/images/Abhiman-SMD%20Alive.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Abhiman-SMD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
